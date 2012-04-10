5c21fadd14735fd0425e899e1d4df553d2bb12ba
Andriy Bun <andr.bun@gmail.com>
2012-04-10 11:45:20 +0200

    Rearranging tree

--- Integrated Model/generate_velocities.m
+++
@@ -1,4 +0,0 @@
-function velocity_array = generate_velocities(spatial, column_height_array, mu, sigma)
-    velocity_array = zeros(size(column_height_array));
-    velocity_array(column_height_array > 0) = lognrnd(mu, sigma, spatial.num_columns, 1);
-end
